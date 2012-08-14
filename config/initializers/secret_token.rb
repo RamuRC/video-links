2d3c89a1a047b18103e13ddfaa622c037b1a8da1
root <root@lwlodarczyk.(none)>
2012-08-14 23:54:10 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Serialki::Application.config.secret_token = '3173e7f81864f2925c2ddb521d53a5b76d4c612ac8c76c2c30e56567c7d811e04c4f84e7f3117243edbca8369ec0655e896a2b2dd5bacadd7fc480bb48e3df27'
